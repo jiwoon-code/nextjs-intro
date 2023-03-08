4188494da4c1a681ea7d4f0a27238af88b6296ec
jiwoon-code <115874237+jiwoon-code@users.noreply.github.com>
2023-03-08 11:26:33 +0900

    redirect

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirect() {
+    return [
+      {
+        source: "/contact",
+        destination: "/form",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
